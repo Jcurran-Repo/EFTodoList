4ecbdfc9049bf89011464875abac894cfca4af32
Jordan Curran <Jorcurr@gmail.com>
2020-07-05 18:09:08 -0500

    Original implementation

---
+++ EFTodoList/Data/EFTodoListContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EFTodoList.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace EFTodoList.Data
+{
+    public class EFTodoContext : DbContext
+    {
+        public static DatabaseFacade DBFacade;
+        public EFTodoContext(DbContextOptions<EFTodoContext> options)
+            : base(options)
+        {
+            DBFacade = Database;
+            this.Database.EnsureCreated();
+            if (ToDoListItems.Count() == 0)
+            {
+                var ListItems = new List<ToDoListItem>
+      {
+            new ToDoListItem(0,"Open Visual Studio",true ),
+            new ToDoListItem(1,"Create a new project",true ),
+            new ToDoListItem(2,"Choose ASP.NET Core Web Application",true ),
+            new ToDoListItem(3,"Name the project",true ),
+            new ToDoListItem(4,"Choose .NET Core",true ),
+            new ToDoListItem(5,"Choose  ASP.NET Core 3.1",true ),
+            new ToDoListItem(6,"Choose MVC Web Application",true ),
+            new ToDoListItem(7,"Design a Todo List",true ),
+            new ToDoListItem(8,"Implement a Todo List using scaffolding",true ),
+            new ToDoListItem(9,"Add list items for testing and examples",true ),
+            new ToDoListItem(10,"Test the new Todo List ",true ),
+            new ToDoListItem(11,"Push the new Web Application to gitHub",true ),
+            new ToDoListItem(12,"Get hired by Priority1",false )
+      };
+                ListItems.ForEach(s => this.ListItem.Add(s));
+                SaveChanges();
+            }
+        }
+        public DbSet<ToDoListItem> ToDoListItems { get; set; }
+        public DbSet<EFTodoList.Models.ToDoListItem> ListItem { get; set; }
+    }
+}
---
+++ EFTodoList/Models/ListItem.cs
@@ -0,0 +1,34 @@
+﻿using EFTodoList.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFTodoList.Models
+{
+    public class ToDoListItem
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Order")]
+        public int ListItemOrder { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Completion Status")]
+        public bool IsComplete { get; set; }
+
+        public ToDoListItem(int listItemOrder, string description, bool isComplete)
+        {
+            ListItemOrder = listItemOrder;
+            Description = description;
+            IsComplete = isComplete;
+        }
+
+        public ToDoListItem()
+        {
+            ListItemOrder = 0;
+            Description = "";
+            IsComplete = false;
+        }
+    }
+}
